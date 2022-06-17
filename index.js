95902c021986cc2b17803c9749c2cccf787752b7
Fershad <itsfish@fershad.com>
2022-06-17 16:33:48 +0800

    remove options

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import * as cheerio from 'cheerio';
 
-const options = {
-  autoload: false,
-  nocookie: true,
-}
-
 const ytIdRegex = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
 
 function getID(url) {
@@ -25,7 +20,7 @@ async function findIframes(req) {
       const params = new URL(src).searchParams.toString()
 
       if (id) {
-        const lite = `<lite-youtube class="${className || ''}" videoid="${id}" ${options.autoload ? 'autoload' : ''} ${options.nocookie ? 'nocookie' : ''} params='${params}'> </lite-youtube>`
+        const lite = `<lite-youtube class="${className || ''}" videoid="${id}" nocookie params='${params}'> </lite-youtube>`
         $(iframe).replaceWith(lite)
       }
     }
